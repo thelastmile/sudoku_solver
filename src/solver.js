d940cab097e30e363d1a930ce5b75b8d8ccfd11f
hschoenburg <hschoenburg@gmail.com>
2016-01-19 08:54:15 -0800

    solution added

--- src/solver.js
+++ src/solver.js
@@ -5,10 +5,7 @@ function Solver() {
     var valid = true;
     console.log(puzzle);
 
-
-    if(puzzle.length < 81) {
-      valid = false;
-    };
+;
 
     var valid_chars = [1,2,3,4,5,6,7,8,9,"_"];
 
@@ -21,5 +18,32 @@ function Solver() {
 
     return valid;
   };
+
+
+  this.validate_solution = function(puzzle) {
+
+    var valid = true;
+
+    var valid_chars = [1,2,3,4,5,6,7,8,9];
+
+    puzzle.forEach(function(x) {
+      if(valid_chars.indexOf(x) == -1) {
+        console.log(x + "mememe");
+        valid = false;
+      }
+    });
+
+
+
+  };
+
+
+  this.validate_length = function(puzzle) {
+
+    if(puzzle.length == 81) {
+      return true;
+    }
+
+  });
 };
 
