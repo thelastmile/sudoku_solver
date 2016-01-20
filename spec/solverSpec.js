63ffc12510dd0d3aa23d1939893434b2c0588d28
hschoenburg <hschoenburg@gmail.com>
2016-01-19 17:54:17 -0800

    everything but solve completed

--- spec/solverSpec.js
+++ spec/solverSpec.js
@@ -18,12 +18,33 @@ var sudoku1_solution = [2,5,8,1,3,6,4,9,7,
                         8,2,6,7,4,5,9,3,1,
                         9,4,3,6,8,1,5,7,2];
 
+  var sudoku2 = [5,6,'_',1,'_','_','_',9,'_',
+                 3,8,'_','_','_','_','_','_','_',
+                 '_',1,'_','_',9,'_','_','_','_',
+                 4,3,'_','_','_','_','_','_','_',
+                 7,'_','_','_','_','_','_','_','_',
+                 '_', '_','_','_',8,2,1,'_',7,
+                 '_','_','_','_',7,5,'_','_',3,
+                 '_','_','_','_','_','_','_',8,'_',
+                 1,'_','_',3,2,'_','_','_',5];
+
+var sudoku2_solution = [5,6,7,1,4,3,2,9,8,
+                        3,8,9,2,5,6,4,7,1,
+                        2,1,4,8,9,7,3,5,6,
+                        4,3,8,7,6,1,5,2,9,
+                        7,2,1,5,3,9,8,6,4,
+                        6,9,5,4,8,2,1,3,7,
+                        8,4,2,9,7,5,6,1,3,
+                        9,5,3,6,1,4,7,8,2,
+                        1,7,6,3,2,8,9,4,5];
+
+
 
 describe('validate_puzzle()', function() {
   var solver = new Solver();
 
   // do this so we dont modify the original
-  var puzzle_copy = sudoku1.slice(0);
+  var puzzle_copy = sudoku2.slice(0);
 
   it('returns false if puzzle is not 81 elements', function() {
     expect(solver.validate_puzzle(puzzle_copy.slice(70))).to.be.false;
@@ -35,35 +56,67 @@ describe('validate_puzzle()', function() {
   });
 
   it('returns true when passed a valid unfinished sudoku puzzle', function() {
-    expect(solver.validate_puzzle(sudoku1)).to.be.true;
+    expect(solver.validate_puzzle(sudoku2)).to.be.true;
   });
 
 });
 
-describe('validate_solution()', function() {
+describe('validate_solution() basics', function() {
 
+  var solver = new Solver();
+
+  // do this so we dont modify the original
+  var solution_copy = sudoku2_solution.slice(0);
 
   it('returns false when passed an unfinished sudoku puzzle', function() {
-
+    expect(solver.validate_solution(solution_copy.slice(70))).to.be.false;
   });
 
-
   it('returns false when passed an invalid sudoku puzzle', function(){ 
-
+    solution_copy[4] = "badstuff";
+    expect(solver.validate_solution(solution_copy)).to.be.false;
   })
 
+  it('returns true when passed a valid finished sudoku puzzle', function() {
+    expect(solver.validate_solution(sudoku1_solution)).to.be.true;
+  });
+});
+
+describe('validate_solution() advanced', function() {
+  var solver = new Solver();
+
+  it('can detect rows that dont add up', function() {
+    // try this twice to be sure
+
+    var solution_copy = sudoku2_solution.slice(0);
+
+    expect(solver.validate_solution(solution_copy)).to.be.true;
+    solution_copy[15] = 3;
+    expect(solver.validate_solution(solution_copy)).to.be.false;
+
+    var solution_copy = sudoku2_solution.slice(0);
+    expect(solver.validate_solution(solution_copy)).to.be.true;
+    solution_copy[35] = 4;
+    expect(solver.validate_solution(solution_copy)).to.be.false;
+  });
+
+  it('can detect columns that dont add up', function() {
+
+    var solution_copy = sudoku2_solution.slice(0);
+    solution_copy[18] = 7;
+    solution_copy[23] = 6;
+    expect(solver.validate_solution(solution_copy)).to.be.false;
+  });
 
 });
 
 
 describe('solve()', function() {
+  var solver = new Solver();
 
   it('returns a valid and complete sudoku puzzle', function() {
-
-  });
-
-  it('returns false if the sudoku is not solveable', function() {
-
+    expect(solver.solve(sudoku2)).to.eql(sudoku2_solution);
+    expect(solver.solve(sudoku1)).to.eql(sudoku1_solution);
   });
 
 });
--- src/solver.js
+++ src/solver.js
@@ -1,49 +1,96 @@
 function Solver() {
 
+  this.solve = function(puzzle) {
+    // figure it out!
+  };
+
 
   this.validate_puzzle = function(puzzle) {
-    var valid = true;
-    console.log(puzzle);
 
-;
+    var length = this.validate_length(puzzle);
 
-    var valid_chars = [1,2,3,4,5,6,7,8,9,"_"];
+    var chars = this.validate_chars(puzzle, [1,2,3,4,5,6,7,8,9,"_"]);
 
-    puzzle.forEach(function(x) {
-      if(valid_chars.indexOf(x) == -1) {
-        console.log(x + "mememe");
-        valid = false;
-      }
-    });
-
-    return valid;
+    return length && chars;
   };
 
 
   this.validate_solution = function(puzzle) {
+    var puzzle_copy = puzzle.slice(0);
 
+    var length = this.validate_length(puzzle_copy);
+
+    var chars = this.validate_chars(puzzle_copy, [1,2,3,4,5,6,7,8,9]);
+
+    var rows = this.validate_rows(puzzle_copy);
+
+    var cols = this.validate_cols(puzzle_copy);
+
+    console.log(rows);
+    return length && chars && rows && cols;
+    
+  };
+
+  this.validate_rows = function(puzzle) {
+    var rows = true;
+
+    var rowStarts = [0,9,18,27,36,45,54,63,72]
+
+    for(var i = 0; i < 9; i++) {
+      var index = i * 9;
+      var row = puzzle.slice(index, index + 9);
+
+      var sum = 0;
+      row.forEach(function(x) {
+        sum += x;
+      });
+
+      if (sum != 45) {
+        rows = false;
+      }
+    }
+    return rows;
+  };
+
+  this.validate_cols = function(puzzle) {
+    var cols = true;
+
+    for(var x = 0; x < 9; x++) {
+      var col_sum = 0;
+
+      for(var y = 0; y < 9; y++) {
+        var index = x + (y * 9);
+        col_sum += puzzle[index];
+      }
+
+      if(col_sum != 45) {
+        cols = false;
+      }
+    }
+    return cols;
+  };
+
+
+  this.validate_chars = function(puzzle, valid_chars) {
     var valid = true;
-
-    var valid_chars = [1,2,3,4,5,6,7,8,9];
-
     puzzle.forEach(function(x) {
       if(valid_chars.indexOf(x) == -1) {
-        console.log(x + "mememe");
         valid = false;
       }
     });
-
-
-
+    return valid;
   };
+
 
 
   this.validate_length = function(puzzle) {
 
     if(puzzle.length == 81) {
       return true;
+    } else {
+      return false;
     }
 
-  });
+  };
 };
 
